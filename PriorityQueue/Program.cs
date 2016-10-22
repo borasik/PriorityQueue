fe9e59476984e88dc674b6af158dd400422e8088
Alex Borsuk <alex.borsuk@dh.com>
2016-10-22 19:09:43 -0400

    Add project files.

---
+++ PriorityQueue/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    public class Program
+    {
+        public static IList<int> List = new List<int>(); 
+        public static void Main(string[] args)
+        {
+           
+            while (true)
+            {
+                Console.WriteLine("Please Enter Number [0 - Exit, 1 to print min]");
+                var num = int.Parse(Console.ReadLine());
+
+                if (num == 0) break;
+
+                if (num == 1) PrintMin(List);
+
+                List.Add(num);
+                Heap.Heapfy(List);
+            }
+
+            Console.Read();
+        }
+
+        private static void PrintMin(IList<int> list)
+        {
+            Console.WriteLine("Min is: " + list[0]);
+        }
+    }
+
+    public class Heap
+    {
+        public static void Heapfy(IList<int> list)
+        { 
+            if (list.Count < 1) return;
+            var leafsIndex = (list.Count - 2) / 2;
+            for(int i = leafsIndex; i >= 0; i--)
+            {
+                var leftChild = i * 2+1;
+                var rightChild = i * 2 + 2;
+
+                if (leftChild > list.Count - 1) continue;
+                if (list[i] > list[leftChild])
+                {
+                    var temp = list[i];
+                    list[i] = list[leftChild];
+                    list[leftChild] = temp;
+                }
+
+                if (rightChild > list.Count - 1) continue;
+                if (list[i] > list[rightChild])
+                {
+                    var temp = list[i];
+                    list[i] = list[rightChild];
+                    list[rightChild] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ PriorityQueueTest/Programm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PriorityQueue;
+
+namespace PriorityQueueTest
+{
+    [TestClass]
+    public class Programm
+    {
+        [TestMethod]
+        public void TestHeapify()
+        {
+            var list = new List<int>() {300, 200};
+            
+            Heap.Heapfy(list);
+
+            Assert.AreEqual(list.Count, 2);
+            CollectionAssert.AreEqual(list, new List<int>() {200, 300});
+        }
+
+        [TestMethod]
+        public void TestHeapifyIfListIsEmpty()
+        {
+            var list = new List<int>();
+
+            Heap.Heapfy(list);
+
+            Assert.AreEqual(list.Count, 0);
+         
+        }
+        [TestMethod]
+        public void TestHeapifyWithBounderyConditions()
+        {
+            var list = new List<int>() { 300, 200, -1, 5, -1 };
+
+            Heap.Heapfy(list);
+
+            Assert.AreEqual(list.Count, 5);
+            CollectionAssert.AreEqual(list, new List<int>() { -1, -1, 5, 300, 200 });
+        }
+    }
+}
